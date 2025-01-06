9cea4bcadb95e4caf75b2576516a773ff2d07d57
FN-TOKENS <84870847+dc199411@users.noreply.github.com>
2025-01-06 15:26:09 -0500

    Initialize cmcewen/sol-fees

---
+++ src/consts/config.ts
@@ -0,0 +1,42 @@
+import { ChainMap } from '@hyperlane-xyz/sdk';
+
+import { ADDRESS_BLACKLIST } from './blacklist';
+
+const isDevMode = process?.env?.NODE_ENV === 'development';
+const version = process?.env?.NEXT_PUBLIC_VERSION || '0.0.0';
+const registryUrl = process?.env?.NEXT_PUBLIC_REGISTRY_URL || undefined;
+const explorerApiKeys = JSON.parse(process?.env?.EXPLORER_API_KEYS || '{}');
+const walletConnectProjectId = process?.env?.NEXT_PUBLIC_WALLET_CONNECT_ID || '';
+const withdrawalWhitelist = process?.env?.NEXT_PUBLIC_BLOCK_WITHDRAWAL_WHITELIST || '';
+const transferBlacklist = process?.env?.NEXT_PUBLIC_TRANSFER_BLACKLIST || '';
+const chainWalletWhitelists = JSON.parse(process?.env?.NEXT_PUBLIC_CHAIN_WALLET_WHITELISTS || '{}');
+
+interface Config {
+  isDevMode: boolean; // Enables some debug features in the app
+  version: string; // Matches version number in package.json
+  registryUrl: string | undefined; // Optional URL to use a custom registry instead of the published canonical version
+  explorerApiKeys: Record<string, string>; // Optional map of API keys for block explorer
+  showTipBox: boolean; // Show/Hide the blue tip box above the transfer form
+  showDisabledTokens: boolean; // Show/Hide invalid token options in the selection modal
+  walletConnectProjectId: string; // Project ID provided by walletconnect
+  withdrawalWhitelist: string; // comma-separated list of CAIP2 chain IDs to which transfers are supported
+  transferBlacklist: string; // comma-separated list of routes between which transfers are disabled. Expects Caip2Id-Caip2Id (e.g. ethereum:1-sealevel:1399811149)
+  enableExplorerLink: boolean; // Include a link to the hyperlane explorer in the transfer modal
+  addressBlacklist: string[]; // A list of addresses that are blacklisted and cannot be used in the app
+  chainWalletWhitelists: ChainMap<string[]>; // A map of chain names to a list of wallet names that work for it
+}
+
+export const config: Config = Object.freeze({
+  isDevMode,
+  version,
+  registryUrl,
+  explorerApiKeys,
+  showTipBox: false,
+  showDisabledTokens: true,
+  walletConnectProjectId,
+  withdrawalWhitelist,
+  transferBlacklist,
+  enableExplorerLink: false,
+  addressBlacklist: ADDRESS_BLACKLIST.map((address) => address.toLowerCase()),
+  chainWalletWhitelists,
+});
---
+++ src/consts/warpRouteWhitelist.ts
@@ -0,0 +1,37 @@
+// A list of warp route config IDs to be included in the app
+// Warp Route IDs use format `SYMBOL/chainname1-chainname2...` where chains are ordered alphabetically
+// If left null, all warp routes in the configured registry will be included
+// If set to a list (including an empty list), only the specified routes will be included
+export const warpRouteWhitelist: Array<string> | null = [
+  // TIA routes
+  'TIA.n/arbitrum-neutron',
+  'TIA.n/mantapacific-neutron',
+
+  // ETH routes
+  'ETH/ethereum-viction',
+
+  // tETH routes
+  'tETH/eclipsemainnet-ethereum',
+
+  // ECLIP routes
+  'ECLIP/arbitrum-neutron',
+
+  // USDC routes
+  'USDC/ethereum-inevm',
+  'USDC/ancient8-ethereum',
+  'USDC/ethereum-viction',
+  'USDC/eclipsemainnet-ethereum-solanamainnet',
+
+  // USDT routes
+  'USDT/ethereum-inevm',
+  'USDT/ethereum-viction',
+
+  // INJ routes
+  'INJ/inevm-injective',
+
+  // SOL routes
+  'SOL/eclipsemainnet-solanamainnet',
+
+  // WIF routes
+  'WIF/eclipsemainnet-solanamainnet',
+];
